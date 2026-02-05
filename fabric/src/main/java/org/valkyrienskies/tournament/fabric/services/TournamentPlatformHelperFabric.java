1480d465c175f1da1f06d64be3d302b9df60099e
Alexander Nutz <alexander.nutz@vxcc.dev>
2026-02-05 16:34:25 +0100

    compiles on fabric now

--- fabric/src/main/java/org/valkyrienskies/tournament/fabric/services/TournamentPlatformHelperFabric.java
+++ fabric/src/main/java/org/valkyrienskies/tournament/fabric/services/TournamentPlatformHelperFabric.java
@@ -2,6 +2,7 @@ package org.valkyrienskies.tournament.fabric.services;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -14,6 +15,6 @@ public class TournamentPlatformHelperFabric implements TournamentPlatformHelper 
     public BakedModel loadBakedModel(@NotNull ResourceLocation modelLocation) {
         return Minecraft.getInstance()
                 .getModelManager()
-                .getModel(modelLocation);
+                .getModel(new ModelResourceLocation(modelLocation, ""));
     }
 }
